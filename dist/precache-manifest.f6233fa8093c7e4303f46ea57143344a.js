f1a7087db936d41f1ffe88a42facf67a4698ad6b
BaptisteMagoni <34521668+BaptisteMagoni@users.noreply.github.com>
2019-10-23 09:32:06 +0200

    Import Test

---
+++ dist/precache-manifest.f6233fa8093c7e4303f46ea57143344a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d538d9ab21f7df0f430",
+    "url": "/css/app.ba629074.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "8030a625d9bc690763b8ef7a685c87d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f8a5831f4e54ff48bc0d",
+    "url": "/js/about.e97b8d0b.js"
+  },
+  {
+    "revision": "6d538d9ab21f7df0f430",
+    "url": "/js/app.090d24ca.js"
+  },
+  {
+    "revision": "18a8dff35c2ee226a844",
+    "url": "/js/chunk-vendors.d47cf30f.js"
+  },
+  {
+    "revision": "a8c976ce1df196bdf9bbe5f3cfb86bee",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
